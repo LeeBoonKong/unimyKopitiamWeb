445d5124ddfcd6cca683a303e20363532bd8a7d2
Lee Boon Kong <lee.boon.kong@hotmail.com>
2018-09-01 13:11:14 +0800

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,43 @@
 var express = require('express');
 var router = express.Router();
-var url="mongodb://localhost:27017"
+var url="mongodb://localhost:27017/test"
 var mongoose = require('mongoose');
 
-mongoose.connect('localhost:27017/test')
+mongoose.connect(url);
 
 var Schema = mongoose.Schema;
 
 var confessionSchema = new Schema({
-  id : {type : Number , required : true},
   content : String,
-  date : Date
-});
+  date : {type: Date, default:Date.now}
+}, {collection: 'confession-data'});
 
-var Confession = mongoose.model('confession', confessionSchema);
-
-
+var Confession = mongoose.model('Confession', confessionSchema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Unimy Kopitiam' });
+  Confession.find()
+  .sort({date: -1})
+  .then(function(doc){
+    res.render('index', {title: 'Unimy Kopitiam', confession: doc});
+  });
 });
 
 router.post('/post-confession', function(req,res){
-
+  var input = {
+    content : req.body.confessionText,
+  };
+  var data = new Confession(input);
+  data.save();
+  res.redirect('/');
 });
 
 router.get('/get-confession', function(req, res, next){
-
+  Confession.find()
+  .limit(20)
+  .then(function(doc){
+    res.render('index', { confession: doc});
+  });
 });
 
 module.exports = router;
